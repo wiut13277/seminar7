76243ffd4c310c6626661fafbd795d5b9c4f17dd
Fazliddin00013277 <wiut13277@gmail.com>
2022-10-27 13:41:00 +0500

    Seminar 7

---
+++ Task.py
@@ -0,0 +1,11 @@
+student = {'name': 'Fazliddin',
+           'module': 'CS_fun',
+           'mark': 80
+          }
+
+eng2uz = dict()
+eng2uz['one'] = 'bir'
+eng2uz['two'] = 'ikki'
+
+print(student)
+print(eng2uz)
