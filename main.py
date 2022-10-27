920b99c6ee05b991ddbe8df93140df8b98fe3d9b
Fazliddin00013277 <wiut13277@gmail.com>
2022-10-27 13:56:56 +0500

    Seminar7

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ eng2uz['two'] = 'ikki'
 
 print(student)
 print(eng2uz)
+print(len(student))
+
+
+
+myFinalMarks = { 'CSF': 75,'FunPro': 80, 'WT': 85 }
+
+def calculateAverage(finalMarks):
+    total = 0
+    for key in finalMarks:
+        total +=finalMarks[key]
+    average = total/len(finalMarks)
+    return average
+
+print(calculateAverage(myFinalMarks))
